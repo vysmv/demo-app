c7abffd2901d8c140a0b25c3ab3f2c5a59713b2b
vysmv <vysmv.contact@gmail.com>
2026-05-11 11:51:54 +0300

    chapter 3.4

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -11,7 +11,7 @@ import (
 // *application.
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
     w.Header().Add("Server", "Go")
-
+    
     files := []string{
         "./ui/html/base.tmpl",
         "./ui/html/partials/nav.tmpl",
@@ -20,22 +20,13 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
     ts, err := template.ParseFiles(files...)
     if err != nil {
-        // Because the home handler is now a method against the application
-        // struct it can access its fields, including the structured logger. We'll 
-        // use this to create a log entry at Error level containing the error
-        // message, also including the request method and URI as attributes to 
-        // assist with debugging.
-        app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
-        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        app.serverError(w, r, err) // Use the serverError() helper.
         return
     }
 
     err = ts.ExecuteTemplate(w, "base", nil)
     if err != nil {
-        // And we also need to update the code here to use the structured logger
-        // too.
-        app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
-        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        app.serverError(w, r, err) // Use the serverError() helper.
     }
 }
 
