4d6492c094f93ab05a9a3f784472e2ae331ee3e9
vysmv <vysmv.contact@gmail.com>
2026-05-11 11:18:31 +0300

    chapter 2.6

--- main.go
+++ main.go
@@ -8,28 +8,27 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+    // Use the Header().Add() method to add a 'Server: Go' header to the
+    // response header map. The first parameter is the header name, and
+    // the second parameter is the header value.
+    w.Header().Add("Server", "Go")
+
     w.Write([]byte("Hello from Snippetbox"))
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-    // Extract the value of the id wildcard from the request using r.PathValue()
-    // and try to convert it to an integer using the strconv.Atoi() function. If
-    // it can't be converted to an integer, or the value is less than 1, we
-    // return a 404 page not found response.
     id, err := strconv.Atoi(r.PathValue("id"))
     if err != nil || id < 1 {
         http.NotFound(w, r)
         return
     }
 
-    // Use the fmt.Sprintf() function to interpolate the id value with a
-    // message, then write it as the HTTP response.
-    msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
-    w.Write([]byte(msg))
+    fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-// Add a snippetCreatePost handler function.
 func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusCreated)
+
     w.Write([]byte("Save a new snippet..."))
 }
 
