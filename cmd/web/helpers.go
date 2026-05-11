b47e05252585e24b4e09a3f172051d319e4adbc3
vysmv <vysmv.contact@gmail.com>
2026-05-11 12:50:20 +0300

    chapter 5.4

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-    "fmt" // New import
+    "bytes" // New import
+    "fmt"
     "net/http"
 )
 
@@ -26,10 +27,6 @@ func (app *application) clientError(w http.ResponseWriter, status int) {
 }
 
 func (app *application) render(w http.ResponseWriter, r *http.Request, status int, page string, data templateData) {
-    // Retrieve the appropriate template set from the cache based on the page
-    // name (like 'home.tmpl'). If no entry exists in the cache with the
-    // provided name, then create a new error and call the serverError() helper
-    // method that we made earlier and return.
     ts, ok := app.templateCache[page]
     if !ok {
         err := fmt.Errorf("the template %s does not exist", page)
@@ -37,13 +34,24 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, status in
         return
     }
 
-    // Write out the provided HTTP status code ('200 OK', '400 Bad Request' etc).
-    w.WriteHeader(status)
+    // Initialize a new buffer.
+    buf := new(bytes.Buffer)
 
-    // Execute the template set and write the response body. Again, if there
-    // is any error we call the serverError() helper.
-    err := ts.ExecuteTemplate(w, "base", data)
+    // Write the template to the buffer, instead of straight to the
+    // http.ResponseWriter. If there's an error, call our serverError() helper
+    // and then return.
+    err := ts.ExecuteTemplate(buf, "base", data)
     if err != nil {
         app.serverError(w, r, err)
+        return
     }
+
+    // If the template is written to the buffer without any errors, it's safe
+    // to go ahead and write the HTTP status code to http.ResponseWriter.
+    w.WriteHeader(status)
+
+    // Write the contents of the buffer to the http.ResponseWriter. Note: this
+    // is another time where we pass our http.ResponseWriter to a function that
+    // takes an io.Writer.
+    buf.WriteTo(w)
 }
