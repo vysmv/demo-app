57888d304ce63a6dac41809aeee99646c7ca7113
vysmv <vysmv.contact@gmail.com>
2026-05-11 11:13:23 +0300

    chapter 2.4

--- main.go
+++ main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+    "fmt" // New import
     "log"
     "net/http"
+    "strconv" // New import
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("Hello from Snippetbox"))
 }
 
-// Add a snippetView handler function.
 func snippetView(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Display a specific snippet..."))
+    // Extract the value of the id wildcard from the request using r.PathValue()
+    // and try to convert it to an integer using the strconv.Atoi() function. If
+    // it can't be converted to an integer, or the value is less than 1, we
+    // return a 404 page not found response.
+    id, err := strconv.Atoi(r.PathValue("id"))
+    if err != nil || id < 1 {
+        http.NotFound(w, r)
+        return
+    }
+
+    // Use the fmt.Sprintf() function to interpolate the id value with a
+    // message, then write it as the HTTP response.
+    msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
+    w.Write([]byte(msg))
 }
 
 // Add a snippetCreate handler function.
@@ -21,8 +35,8 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     mux := http.NewServeMux()
-    mux.HandleFunc("/{$}", home) // Restrict this route to exact matches on / only.
-    mux.HandleFunc("/snippet/view", snippetView) 
+    mux.HandleFunc("/{$}", home)
+    mux.HandleFunc("/snippet/view/{id}", snippetView)  // Add the {id} wildcard segment
     mux.HandleFunc("/snippet/create", snippetCreate)
 
     log.Print("starting server on :4000")
