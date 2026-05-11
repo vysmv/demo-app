54feaa85115c690aaf33f173faf60c735cc9d649
vysmv <vysmv.contact@gmail.com>
2026-05-11 14:00:53 +0300

    chapter 9.5

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls" // New import
 	"database/sql"
 	"flag"
 	"time" // New import
@@ -11,8 +12,8 @@ import (
 	"os"
 
 	"github.com/alexedwards/scs/mysqlstore"
-	"github.com/alexedwards/scs/v2"                      // New import
-	"github.com/vysmv/snippetbox-public/internal/models" // New import
+	"github.com/alexedwards/scs/v2"
+	"github.com/vysmv/snippetbox-public/internal/models"
 
 	"github.com/go-playground/form/v4"
 	_ "github.com/go-sql-driver/mysql"
@@ -73,24 +74,33 @@ func main() {
 		sessionManager: sessionManager,
 	}
 
+	// Initialize a tls.Config struct to hold the non-default TLS settings we
+	// want the server to use. In this case the only thing that we're changing
+	// is the curve preferences value, so that only elliptic curves with
+	// assembly implementations are used.
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+	}
+
 	srv := &http.Server{
-        Addr:     *addr,
-        Handler:  app.routes(),
-        // Create a *log.Logger from our structured logger handler, which writes
-        // log entries at the Error level, and assign it to the ErrorLog field. If 
-        // you would prefer to log the server errors at Warn level instead, you
-        // could pass slog.LevelWarn as the final parameter.
-        ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
-    }
+		Addr:    *addr,
+		Handler: app.routes(),
+		// Create a *log.Logger from our structured logger handler, which writes
+		// log entries at the Error level, and assign it to the ErrorLog field. If
+		// you would prefer to log the server errors at Warn level instead, you
+		// could pass slog.LevelWarn as the final parameter.
+		ErrorLog:  slog.NewLogLogger(logger.Handler(), slog.LevelError),
+		TLSConfig: tlsConfig,
+	}
 
-    logger.Info("starting server", "addr", srv.Addr)
+	logger.Info("starting server", "addr", srv.Addr)
 
-    // Use the ListenAndServeTLS() method to start the HTTPS server. We
-    // pass in the paths to the TLS certificate and corresponding private key as
-    // the two parameters.
-    err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
-    logger.Error(err.Error())
-    os.Exit(1)
+	// Use the ListenAndServeTLS() method to start the HTTPS server. We
+	// pass in the paths to the TLS certificate and corresponding private key as
+	// the two parameters.
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
+	logger.Error(err.Error())
+	os.Exit(1)
 }
 
 // The openDB() function wraps sql.Open() and returns a sql.DB connection pool
