32d525d321ecdb90de2d551449fc77a5ee6690c9
vysmv <vysmv.contact@gmail.com>
2026-05-11 14:40:53 +0300

    chapter 12.2

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"html/template"
+	"io/fs" // New import
 	"path/filepath"
-	"time" // New import
+	"time"
 
 	"github.com/vysmv/demo-app/internal/models"
+	"github.com/vysmv/demo-app/ui" // New import
 )
 
 // Include a Snippets field in the templateData struct.
@@ -33,40 +35,37 @@ func humanDate(t time.Time) string {
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
-	cache := map[string]*template.Template{}
+    cache := map[string]*template.Template{}
 
-	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
-	if err != nil {
-		return nil, err
-	}
+    // Use fs.Glob() to get a slice of all filepaths in the ui.Files embedded
+    // filesystem which match the pattern 'html/pages/*.tmpl'. This essentially
+    // gives us a slice of all the 'page' templates for the application, just
+    // like before.
+    pages, err := fs.Glob(ui.Files, "html/pages/*.tmpl")
+    if err != nil {
+        return nil, err
+    }
 
-	for _, page := range pages {
-		name := filepath.Base(page)
+    for _, page := range pages {
+        name := filepath.Base(page)
 
-		// The template.FuncMap must be registered with the template set before you
-		// call the ParseFiles() method. This means we have to use template.New() to
-		// create an empty template set, use the Funcs() method to register the
-		// template.FuncMap, and then parse the file as normal.
-		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
-		if err != nil {
-			return nil, err
-		}
+        // Create a slice containing the filepath patterns for the templates we
+        // want to parse.
+        patterns := []string{
+            "html/base.tmpl",
+            "html/partials/*.tmpl",
+            page,
+        }
 
-		// Call ParseGlob() *on this template set* to add any partials.
-		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
-		if err != nil {
-			return nil, err
-		}
+        // Use ParseFS() instead of ParseFiles() to parse the template files 
+        // from the ui.Files embedded filesystem.
+        ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
+        if err != nil {
+            return nil, err
+        }
 
-		// Call ParseFiles() *on this template set* to add the  page template.
-		ts, err = ts.ParseFiles(page)
-		if err != nil {
-			return nil, err
-		}
+        cache[name] = ts
+    }
 
-		// Add the template set to the map as normal...
-		cache[name] = ts
-	}
-
-	return cache, nil
+    return cache, nil
 }
